6a74bedb6e4f1c682bdfcde7b57eb7b3940de535
Raiyan109 <kraiyan109@gmail.com>
2024-01-05 20:31:01 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
-
+import { CiSearch } from "react-icons/ci";
+import { GoPerson } from "react-icons/go";
+import '../styles/navbar.css'
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="flex justify-around items-center  px-5 py-8">
+            <h1 className="font-bold text-xl">pti.</h1>
+            <div className="flex">
+                <div className="flex items-center bg-white px-2 h-8 rounded-md">
+                    <CiSearch className="text-orange-600" />
+                    <input type="text"
+                        className="textInput"
+                        placeholder="Search Audiobook"
+                    />
+                </div>
+                <div>
+                    <select
+                        name="sort"
+                        className="select"
+                    >
+                        <option value="home">Home</option>
+                        <option value="details">Details</option>
+                        <option value="category">Category</option>
+                        <option value="myFavorites">My Favorites</option>
+                        <option value="profile">Profile</option>
+                        <option value="login/sign up">Log in/Sign Up</option>
+                    </select>
+                </div>
+            </div>
+            <div className="bg-orange-600 text-white w-7 h-7   rounded-full  md:flex justify-center items-center hidden">
+                <GoPerson />
+            </div>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
     return (
-        <div>
+        <div className="">
             <Navbar />
         </div>
     );
