aa451ead365d362aaeaa66a490c20aa708a56b2a
Raiyan109 <kraiyan109@gmail.com>
2024-01-06 18:26:06 +0600

    popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,25 @@
-
+import Foods from '../../public/foods.json'
 const Popular = () => {
     return (
-        <div>
-
+        <div className="flex justify-center items-center mx-auto">
+            <div>
+                <div className="flex justify-between items-center">
+                    <h1>Popular</h1>
+                    <div>
+                        <h3 className="text-orange-400">Add More</h3>
+                    </div>
+                </div>
+                <div className='flex flex-wrap gap-8'>
+                    {
+                        Foods.map((food) => (
+                            <div key={food.id}>
+                                <img src={food.image} alt="" className='w-56 h-60 object-cover rounded-xl' />
+                                <h5 className='text-center text-gray-500'>{food.name}</h5>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Popular from "../components/Popular";
 
 const Home = () => {
     return (
         <div className="space-y-20">
             <Navbar />
             <Hero />
+            <Popular />
             <Footer />
         </div>
     );
