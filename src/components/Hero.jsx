dc2f8197138c6fe65b754f4a286fe0b2049675fc
Raiyan109 <kraiyan109@gmail.com>
2024-01-07 14:42:58 +0600

    navbar bottom border

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import heroImg from '../assets/Image1.png'
 const Hero = () => {
     return (
-        <div className="flex justify-center items-center">
-            <div className="bg-orange-400 rounded-lg flex py-5 px-10 h-48">
+        <div className="flex justify-center items-center ">
+            <div className="bg-amber-500 rounded-lg flex py-5 px-10 h-[350px] relative w-3/4">
                 <div className='max-w-sm space-y-4'>
                     <h1 className='text-white text-4xl'>Deliver Food To Your Door Step</h1>
                     <h4 className='text-white font-thin text-xl'>Authentic Food, Quick Service, Fast Delivery</h4>
                 </div>
-                <div>
-                    <img src={heroImg} className='w-full h-full object-contain' alt="" />
+                <div className=''>
+                    <img src={heroImg} className='w-[400px] h-[400px] object-contain' alt="" />
                 </div>
             </div>
 
