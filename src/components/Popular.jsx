b8889c550e3b6fcab2a46bd6b5a2dd4dfa45b77e
Raiyan109 <kraiyan109@gmail.com>
2024-01-07 14:34:29 +0600

    slider gap

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -23,15 +23,15 @@ const Popular = () => {
                 breakpoints={{
                     640: {
                         slidesPerView: 2,
-                        spaceBetween: 20,
+                        spaceBetween: 10,
                     },
                     768: {
                         slidesPerView: 4,
-                        spaceBetween: 40,
+                        spaceBetween: 20,
                     },
                     1024: {
                         slidesPerView: 5,
-                        spaceBetween: 50,
+                        spaceBetween: 10,
                     },
                 }}
                 modules={[Pagination]}
--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -24,15 +24,15 @@ const Recommended = () => {
                 breakpoints={{
                     640: {
                         slidesPerView: 2,
-                        spaceBetween: 20,
+                        spaceBetween: 10,
                     },
                     768: {
                         slidesPerView: 4,
-                        spaceBetween: 40,
+                        spaceBetween: 20,
                     },
                     1024: {
                         slidesPerView: 5,
-                        spaceBetween: 50,
+                        spaceBetween: 10,
                     },
                 }}
                 modules={[Pagination]}
