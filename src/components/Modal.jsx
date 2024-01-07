383fbae48b2d1f6b3be166c669c6b2df473f06e9
Raiyan109 <kraiyan109@gmail.com>
2024-01-07 22:30:03 +0600

    form validation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useRef } from "react";
 import { FaX } from "react-icons/fa6";
 import { Food_Context } from "../context/FoodProvider";
 import { useForm } from 'react-hook-form'
@@ -8,11 +8,11 @@ import { DevTool } from "@hookform/devtools";
 const Modal = ({ setShowPopup, showPopup }) => {
 
     const form = useForm()
-    const { register, control } = form
-    const [formData, setFormData] = useState({
-        Name: '',
-        ImageUrl: '',
-    });
+    const { register, control, handleSubmit, formState: { errors } } = form
+    // const [formData, setFormData] = useState({
+    //     Name: '',
+    //     ImageUrl: '',
+    // });
     const { foods, setFoods } = useContext(Food_Context)
 
     const popupRef = useRef()
@@ -23,26 +23,23 @@ const Modal = ({ setShowPopup, showPopup }) => {
         }
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // Add validation logic here before adding to the list
-
-        // Update the list of items with the new item from the form
-        setFoods([...foods, formData]);
-
-        // Reset the form fields after adding the item
-        setFormData({
-            Name: '',
-            ImageUrl: '',
-            // Reset other fields as required
-        });
+    const onSubmit = (data) => {
+        setFoods([...foods, data]);
     }
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setFormData({ ...formData, [name]: value });
-        console.log(formData);
-    }
+    // const handleFormSubmit = (e) => {
+    //     e.preventDefault()
+
+    //     setFoods([...foods, formData]);
+
+
+    // }
+
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target
+    //     setFormData({ ...formData, [name]: value });
+    //     console.log(formData);
+    // }
 
     return (
         <div ref={popupRef} onClick={closePopup} className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-10">
@@ -52,26 +49,30 @@ const Modal = ({ setShowPopup, showPopup }) => {
                     onClick={() => setShowPopup(!showPopup)}><FaX /></button>
                 <div className="flex justify-center items-center bg-orange-500 px-20 py-10 flex-col gap-5 mx-4 rounded-xl">
                     <h1 className="text-white text-2xl">Add New Item</h1>
-                    <form className="space-y-10" onSubmit={handleSubmit}>
+                    <form className="space-y-10" onSubmit={handleSubmit(onSubmit)}>
                         <input
                             type="text"
                             placeholder="Item Name"
                             className="w-full h-12 px-4 text-black border-gray-300 rounded-md"
-                            value={formData.Name}
-                            // name="Name"
-                            {...register("Name")}
-                            id="name"
-                            onChange={handleChange}
+                            // value={formData.Name}
+                            name="Name"
+                            {...register("Name", { required: true })}
+                            id="Name"
+                        // onChange={handleChange}
                         />
+                        {errors.Name?.type === "required" && (
+                            <p className="text-xs">Name is required</p>
+                        )}
                         <input type="text"
                             placeholder="Item Image Url"
                             className="w-full h-12 px-4 text-black border-gray-300 rounded-md"
-                            value={formData.ImageUrl}
-                            // name="ImageUrl"
-                            {...register("ImageUrl")}
-                            id="imageUrl"
-                            onChange={handleChange}
+                            // value={formData.ImageUrl}
+                            name="ImageUrl"
+                            {...register("ImageUrl", { required: "Image Url is required" })}
+                            id="ImageUrl"
+                        // onChange={handleChange}
                         />
+                        {errors.ImageUrl && <p className="text-xs">{errors.ImageUrl.message}</p>}
                         <div className="flex items-center justify-center">
                             <button type="submit" className='bg-amber-400 text-orange-700 rounded-lg text-lg font-bold md:font-normal py-1 px-7 '>
                                 Add
